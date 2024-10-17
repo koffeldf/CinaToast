b39e57f6038d508a211ee53a21ad6a197604eab4
kọfị <111256281+koffeldf@users.noreply.github.com>
2024-10-17 01:40:25 -0500

    Delete Cinna_Toast_Game directory

--- Cinna_Toast_Game/menu.js
+++
@@ -1,9 +0,0 @@
-var audio = new Audio('Start.mp3');
-
-document.getElementById("SB").onclick = function(){
-    audio.play();
-    audio.onended = function() {
-        window.location.replace("game.html");
-    };
-}
-
